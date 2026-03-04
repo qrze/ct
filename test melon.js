5fe4cd8f105878e11471fec49bc30ffa5668d7ef
melon-vr <angusp543@gmail.com>
2026-03-04 13:08:11 +0000

    Update test melon.js

--- test melon.js
+++ test melon.js
@@ -7,7 +7,7 @@ var id = "adaptive_multi_regime";
 var name = "Adaptive Multi-Regime Stability";
 var description = "Stable equilibrium growth with smooth resonance dynamics.";
 var authors = "qrze, melon";
-var version = 4.3;
+var version = 4.4;
 
 requiresGameVersion("1.4.33");
 
